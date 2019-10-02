7ecd1655a25fee53695906395bceb19c1f9e200e
CarolinePepinNeang <46560897+CarolinePepinNeang@users.noreply.github.com>
2019-10-02 17:47:37 +0200

    FINAL SCHEMA SQL
    Création des différentes tables de la base de données
    - timeslots
    - users
    - productions
    - production-fields

--- SCHEMA electricty_carbon_footprint.sql
+++ SCHEMA electricty_carbon_footprint.sql
@@ -1,77 +1,69 @@
 #################################################################
-#################################################################
 ### PROJET CERTIFICATION : ELECTRIC CARBON FOOTPRINT _ SCHEMA ###
 #################################################################
-#################################################################
-
 
 DROP SCHEMA IF EXISTS ElectricOrigin;
 CREATE DATABASE ElectricOrigin CHARACTER SET utf8;
 USE ElectricOrigin;
 
-#############
-## MaConso ##
-#############
-DROP TABLE IF EXISTS MaConso;
-CREATE TABLE MaConso(
-	date_hour DATETIME
-	,consumption_hour INT
+##################
+## consumptions ##
+##################
+DROP TABLE IF EXISTS consumptions;
+CREATE TABLE consumptions(
+	timeslot DATETIME NOT NULL
+	,user_id INT NOT NULL
+	,consumption INT NOT NULL
+	,carbon_footprint DECIMAL(6,3) NULL
+	,PRIMARY KEY(timeslot, user_id)
 );
 
+###############
+## timeslots ##
+###############
+DROP TABLE IF EXISTS timeslots;
+CREATE TABLE timeslots(
+	timeslot DATETIME NOT NULL
+	,season ENUM('spring','summer','fall','winter') NULL
+	,day_night ENUM('day', 'night') NULL
+	,PRIMARY KEY(timeslot)
+);
+ALTER TABLE consumptions ADD CONSTRAINT fk_consumptions_timeslot FOREIGN KEY(timeslot) REFERENCES timeslots(timeslot);
+
+###########
+## users ##
+###########
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	user_id INT  NOT NULL
+	,lastname VARCHAR(20) NULL
+	,firstname VARCHAR(20) NULL
+	,mail VARCHAR(40) NULL
+	,password TEXT NULL
+	,number_people TINYINT NULL DEFAULT 1
+	,PRIMARY KEY(user_id)
+);
+ALTER TABLE consumptions ADD CONSTRAINT fk_consumptions_user_id FOREIGN KEY(user_id) REFERENCES users(user_id);
+
+#################
+## productions ##
+#################
+DROP TABLE IF EXISTS productions;
+CREATE TABLE productions(
+	timeslot DATETIME NOT NULL
+	,production_field VARCHAR(50) NOT NULL
+	,production INT NOT NULL
+	,PRIMARY KEY(timeslot, production_field)
+);
+ALTER TABLE productions ADD CONSTRAINT fk_productions_timeslot FOREIGN KEY(timeslot) REFERENCES timeslots(timeslot);
 
 #######################
-## ProductionFiliere ##
+## production_fields ##
 #######################
-DROP TABLE IF EXISTS ProductionFiliere;
-CREATE TABLE ProductionFiliere(
-	date_hour DATETIME
-	,BIOMASS INT
-	,FOSSIL_GAS INT
-	,FOSSIL_HARD_COAL INT
-	,FOSSIL_OIL INT
-	,HYDRO_PUMPED_STORAGE INT
-	,HYDRO_RUN_OF_RIVER_AND_POUNDAGE INT
-	,HYDRO_WATER_RESERVOIR INT
-	,NUCLEAR INT
-	,SOLAR INT
-	,WASTE INT
-	,WIND_ONSHORE INT
-	,TOTAL INT
+DROP TABLE IF EXISTS production_fields;
+CREATE TABLE production_fields(
+	name VARCHAR(50) NOT NULL
+	,C02_tons_per_MWh DECIMAL(4,3) NOT NULL
+	,PRIMARY KEY(name)
 );
-
-
-#################
-## EmissionCO2 ##
-#################
-DROP TABLE IF EXISTS EmissionCO2;
-CREATE TABLE EmissionCO2(
-	filiere VARCHAR(7)
-	,contribution DECIMAL(4,3) 
-);
-
-
-#################
-## Utilisateur ##
-#################
-DROP TABLE IF EXISTS Utilisateur;
-CREATE TABLE Utilisateur(
-	utilisateur_id INT UNSIGNED AUTO_INCREMENT
-	,nom VARCHAR(20)
-	,prenom VARCHAR(20)
-	,mail VARCHAR (40)
-	,password CHAR(40)  CHARACTER SET ASCII NOT NULL
-	,composition_menage TINYINT
-	,PRIMARY KEY(utilisateur_id)
-);
-
-
-#####################
-## CarbonFootprint ##
-#####################
-DROP TABLE IF EXISTS CarbonFootprint;
-CREATE TABLE CarbonFootprint(
-	carbonfootprint_id INT UNSIGNED AUTO_INCREMENT
-	,poids DECIMAL(6,3)
-	,utilisateur_id INT UNSIGNED
-	,PRIMARY KEY(carbonfootprint_id)
-);
+ALTER TABLE productions ADD CONSTRAINT fk_productions_productionfields FOREIGN KEY(production_field) REFERENCES production_fields(name);
